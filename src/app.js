299037704a26c01237c9db78830fafbdaea19e1f
Jacob Tyrrell <jacobtyrrell@icloud.com>
2020-05-12 00:52:18 +0100

    expanded on express web server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 const express = require('express'); // import express package
+const path = require('path'); // import path package
+const createError = require('http-errors'); //import http-errors package
+
 const app = express(); // initalise 
 const port = 3001; // this is for nginx's proxy
 
-app.get('/', (req, res) => {
-    res.send('Hello! This is a Express server!')
+// Configure the views
+app.set('views', path.join(__dirname, 'views/templates/')) // set templates directory
+app.set('view engine', 'ejs'); // set view engine
+app.set('view options', { pretty : true }); // prettify the code
+
+// Set static directory
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// passes it onto stuff, i guess.
+app.use((req, res, next) => {
+    next();
 });
 
+// Set middleware
+app.use('/', require('./routes/index')); // set index middleware
+
+// Catch 404 and pass on to error handler. 
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+ // Set Error Handler
+app.use((err, req, res, next) => {
+    // Render the error page
+    res.status(err.status || 500);
+    res.render('error', { title: 'Error', status: err.status, message: err.message });
+});
+
+// Initalise the web server
 app.listen(port, () => {
     console.log(`Example application listening on port ${port}`)
 });
